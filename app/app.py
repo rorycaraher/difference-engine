ce77a6aca4e70dc734677ff2d0c6108c81637dc2
Rory Caraher <caraher.rory@gmail.com>
2024-10-25 12:52:16 +0200

    basic db

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ import os
 from flask_cors import CORS
 import json
 from datetime import datetime
+import uuid
+import psycopg2
 # mixdown stuff
 from mixer import mixer
 
@@ -16,23 +18,30 @@ mixer = mixer.Mixer()
 stems_dir = "/Users/rca/nltl/lvg-bucket/mp3/first-principles"
 output_dir = "./output"
 
-
-@app.route('/test_values', methods=['POST'])
-def test_values():
-    data = request.get_json()
-    if 'lvg_values' in data:
-        stems = data['lvg_values']['stems']
-        volumes = data['lvg_values']['volumes'][0:len(stems)] # only need volume for each stem
-        rounded_volumes = [round(i, 3) for i in volumes]
-        print(f"Worked! {rounded_volumes}")
-        return jsonify({"message": f"Success! {data}"}), 200
-    else:
-        return jsonify({"error": "Invalid data"}), 400
+# db connection
+conn = psycopg2.connect(
+    dbname="lvg",
+    user="dbuser",
+    password="dbpass",
+    host="localhost",
+    port="5432"
+)
 
 @app.route('/test_mixdown', methods=['POST'])
 def test_mixdown():
     data = request.get_json()
     if 'lvg_values' in data:
+        job_id = str(uuid.uuid4())
+        timestamp = datetime.now()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO lvg_requests (job_id, timestamp, values) VALUES (%s, %s, %s)
+        """, (job_id, timestamp , json.dumps(data['lvg_values'])))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
         stems = data['lvg_values']['stems']
         volumes = data['lvg_values']['volumes'][0:len(stems)] # only need volume for each stem
         rounded_volumes = [round(i, 3) for i in volumes]
--- app/site/script.js
+++ app/site/script.js
@@ -24,7 +24,46 @@ function generateRandomNumbers() {
     console.log(lvg_values);
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://127.0.0.1:5000/test_mixdown", true);
+    xhr.open("POST", "http://localhost:5000/test_mixdown", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({ lvg_values: lvg_values }));
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert("Worked!");
+        } else {
+            alert("Something wrong!");
+        }
+    };
+}
+
+
+function testDatabase() {
+
+    const stems = Array.from({length: 8}, (_, i) => i + 1);
+    const volumes = [];
+
+    // Shuffle the array to randomize the order
+    for (let i = stems.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [stems[i], stems[j]] = [stems[j], stems[i]];
+    }
+    const selectedStems = stems.slice(0, 3);
+
+    // generate a bunch of random values
+    for (let i = 32 - 1; i > 0; i--) {
+        volumes.push(Math.random())
+    }
+
+    // print the value
+    const lvg_values = {
+        stems: selectedStems,
+        volumes: volumes
+    }
+
+    console.log(lvg_values);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:5000/test_database", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify({ lvg_values: lvg_values }));
     xhr.onload = function() {
